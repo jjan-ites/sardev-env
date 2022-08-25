d6f7fa0fbc1a64a61022f1761dff6e992f19374b
jjan-ites <joren.janssens@telenet.be>
2022-08-25 14:24:30 +0200

    1 file modified

--- src/pages/AdminPage.vue
+++ src/pages/AdminPage.vue
@@ -1,30 +1,28 @@
 <template>
   <q-page class="column flex-center">
-      <div class="actionBtn">
-        <q-btn-dropdown color="primary" label="Actions">
-          <q-list>
-            <q-item clickable v-close-popup @click="onItemClick">
-              <q-item-section>
-                <q-item-label>Create</q-item-label>
-              </q-item-section>
-            </q-item>
+    <div class="actionBtn">
+      <q-btn-dropdown color="primary" label="Actions">
+        <q-list>
+          <q-item clickable v-close-popup @click="onItemClick">
+            <q-item-section>
+              <q-item-label>Create</q-item-label>
+            </q-item-section>
+          </q-item>
 
-            <q-item clickable v-close-popup @click="onItemClick">
-              <q-item-section>
-                <q-item-label>Edit</q-item-label>
-              </q-item-section>
-            </q-item>
+          <q-item clickable v-close-popup @click="onItemClick">
+            <q-item-section>
+              <q-item-label>Edit</q-item-label>
+            </q-item-section>
+          </q-item>
 
-            <q-item clickable v-close-popup @click="onItemClick">
-              <q-item-section>
-                <q-item-label>Delete</q-item-label>
-              </q-item-section>
-            </q-item>
-          </q-list>
-        </q-btn-dropdown>
+          <q-item clickable v-close-popup @click="onItemClick">
+            <q-item-section>
+              <q-item-label>Delete</q-item-label>
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-btn-dropdown>
     </div>
-    <div>
-      
-    </div>
+    <div></div>
   </q-page>
 </template>
